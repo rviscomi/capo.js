8c2b90e6930e5bea21affebb0b85638e321f6e75
Rick Viscomi <rviscomi@gmail.com>
2023-06-19 11:13:03 -0400

    crx base

--- crx/chrome.js
+++ crx/chrome.js
@@ -108,7 +108,7 @@ function capo() {
   const LOGGING_PREFIX = 'Capo: ';
   
   function isMeta(element) {
-    return element.matches('meta:is([charset], [http-equiv], [name=viewport])');
+    return element.matches('meta:is([charset], [http-equiv], [name=viewport], base)');
   }
   
   function isTitle(element) {
