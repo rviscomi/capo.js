b334d7d063486a7918fc3ba5f8157a0c0406775e
Rick Viscomi <rviscomi@gmail.com>
2023-06-22 22:21:03 -0400

    init docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'capo.js',
+      social: {
+        github: 'https://github.com/rviscomi/capo.js',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});
