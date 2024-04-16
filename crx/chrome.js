3af5553776d4b90f5c8075a22d6bf524784e240d
rviscomi <rviscomi@gmail.com>
2024-04-16 15:56:13 -0400

    serialize OT expiry

--- crx/chrome.js
+++ crx/chrome.js
@@ -2,28 +2,28 @@ init();
 
 async function getCurrentTab() {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  return {tabId: tab.id};
+  return { tabId: tab.id };
 }
 
 async function init() {
-  await chrome.storage.local.remove('data');
+  await chrome.storage.local.remove("data");
 
   await chrome.scripting.executeScript({
     target: await getCurrentTab(),
-    files: ['capo.js']
-  })
+    files: ["capo.js"],
+  });
 
   chrome.storage.onChanged.addListener((changes) => {
-    console.log('Storage changed', changes)
-    const {data} = changes;
+    console.log("Storage changed", changes);
+    const { data } = changes;
     if (data?.newValue) {
       print(data.newValue);
     }
   });
 }
-  
+
 function print(result) {
-  console.log('Data', result);
+  console.log("Data", result);
   let frag = document.createDocumentFragment();
   for (let r of result.actual) {
     frag.appendChild(getCapoHeadElement(r));
@@ -38,13 +38,20 @@ function print(result) {
     frag.appendChild(getCapoHeadElement(r));
   }
   sorted.appendChild(frag);
-  document.body.addEventListener('click', handleCapoClick);
+  document.body.addEventListener("click", handleCapoClick);
 }
 
-function getCapoHeadElement({weight, color, selector, innerHTML, isValid, customValidations}) {
-  const span = document.createElement('span');
-  span.classList.add('capo-head-element');
-  span.classList.toggle('invalid', !isValid);
+function getCapoHeadElement({
+  weight,
+  color,
+  selector,
+  innerHTML,
+  isValid,
+  customValidations,
+}) {
+  const span = document.createElement("span");
+  span.classList.add("capo-head-element");
+  span.classList.toggle("invalid", !isValid);
   span.dataset.weight = weight;
   span.style.backgroundColor = color;
   span.dataset.selector = selector;
@@ -55,17 +62,21 @@ function getCapoHeadElement({weight, color, selector, innerHTML, isValid, custom
 }
 
 async function handleCapoClick(event) {
-  const {weight, selector, innerHTML} = event.target.dataset;
+  const { weight, selector, innerHTML } = event.target.dataset;
   const customValidations = JSON.parse(event.target.dataset.customValidations);
-  const isValid = !event.target.classList.contains('invalid');
+  const isValid = !event.target.classList.contains("invalid");
 
   await chrome.storage.local.set({
     click: JSON.stringify({
-      weight, selector, innerHTML, isValid, customValidations
-    })
+      weight,
+      selector,
+      innerHTML,
+      isValid,
+      customValidations,
+    }),
   });
   await chrome.scripting.executeScript({
     target: await getCurrentTab(),
-    files: ['capo.js']
+    files: ["capo.js"],
   });
 }
